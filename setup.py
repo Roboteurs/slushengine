d0b54e165f8c39ed2ac70829f47e881eec3b89f0
Mango-kid <reinermschmidt@gmail.com>
2016-06-13 09:50:08 -0400

    fixed extension error

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1d',
     packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
