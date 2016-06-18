9f357bca9be7d87f1e24f33e9e3410e9d61eb1a3
mangokid <reinerschmidt@roboteurs.com>
2016-06-18 13:52:59 +0000

    changed pinout of reset

--- Slush/Boards/SlushEngine_ModelX.py
+++ Slush/Boards/SlushEngine_ModelX.py
@@ -15,13 +15,13 @@ MTR3_Busy 	= 19
 
 MTR0_ChipSelect 	= 24
 MTR1_ChipSelect 	= 25
-MTR2_ChipSelect 	= 20
+MTR2_ChipSelect 	= 26
 MTR3_ChipSelect 	= 27
 
 MTR_Flag	= 13
 
 '''MCP23 pin defines'''
-MCP23_Reset = 26
+MCP23_Reset = 20
 
 MCP23_IntA	= 21
 MCP23_IntB	= 22
